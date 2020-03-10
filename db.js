6f16a4b33d1eb9adf97611324fe22e5829f956ae
Kelli Warner <kbwarner@gmail.com>
2020-03-09 20:38:04 -0400

    first draft

--- db.js
+++ db.js
@@ -1,13 +1,26 @@
 const pg = require('pg');
+const uuid = require('uuid');
 const client = new pg.Client(
-  process.env.DATABASE_URL || 'postgres://localhost/year_in_pixels_db'
+  process.env.DATABASE_URL || 'postgres://localhost/acme_schools'
 );
 
 client.connect();
 
 const sync = async () => {
   const SQL = `
+  DROP TABLE IF EXISTS student;
+  DROP TABLE IF EXISTS school;
 
+  CREATE TABLE school(
+    id UUID PRIMARY KEY,
+    name VARCHAR (100) NOT NULL
+  );
+
+  CREATE TABLE student(
+    id UUID PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    "schoolId" UUID REFERENCES school(id) DEFAULT NULL
+  );
 `;
   await client.query(SQL);
 };
--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React, { useState, useEffect } from 'react';
 
 const App = () => {
-  return <h1>Hello world</h1>;
+  return (
+    <div className="app">
+      <div className="main-container">
+        <h1>Acme Schools</h1>
+        <div className="forms">
+          <div className="create-student">
+            <form>
+              Create Student
+              <input />
+              <select className="form-control" id="students">
+                <option value="">Student</option>
+              </select>
+              <button>Create</button>
+            </form>
+          </div>
+          <div className="create-school">
+            <form>
+              Create School
+              <input />
+              <button>Create</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
